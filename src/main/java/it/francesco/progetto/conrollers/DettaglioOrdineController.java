870d7d5748d45186a4aa5c259f8e994ce9668c93
francescoblefari <francescoblefari1998@gmail.com>
2020-06-05 08:03:02 +0200

    Add DettaglioOrdineController.stampa()

--- src/main/java/it/francesco/progetto/conrollers/DettaglioOrdineController.java
+++ src/main/java/it/francesco/progetto/conrollers/DettaglioOrdineController.java
@@ -1,4 +1,11 @@
 package it.francesco.progetto.conrollers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DettaglioOrdineController {
+
+    @GetMapping
+    public String stampa(){return "ciao";}
 }
